7203722c9ce6eee588ac0a2463d969112364ac10
mingz <305603665@qq.com>
2016-12-24 18:35:16 +0800

    init

---
+++ app/constants/events.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zhaojm on 24/12/2016.
+ */
+
+const E_OPEN = 'open';
+const E_CLOSE = 'close';
+const E_LOGIN = 'login';
+const E_REGISTER = 'register';
+const E_SIGNIN = 'signin';
